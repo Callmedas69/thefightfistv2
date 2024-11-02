a23e0f07c78912a878f1b22085b916dd651adb89
0xDas <126429597+Callmedas69@users.noreply.github.com>
2024-11-03 02:54:59 +0700

    Update Hero.tsx

--- src/components/mainpage/Hero.tsx
+++ src/components/mainpage/Hero.tsx
@@ -45,25 +45,23 @@ const Hero = () => {
 
       {/* Content Container */}
       <div className="relative flex flex-col lg:flex-row items-center justify-center lg:justify-between max-w-[1100px] w-full h-full px-4 lg:px-0 lg:space-x-10 space-y-6 lg:space-y-0">
-        {/* Launch Image and Button */}
-        <div className="pl-[10%]">
-          <div className="flex flex-col items-center z-40 space-y-4">
-            <Image
-              src={launch}
-              alt="launch"
-              className="w-[20vw] max-w-[200px] lg:max-w-[250px] h-auto" // Updated width classes for scaling
-            />
-            <Button
-              onClick={scrollToSection}
-              className="justify-center items-center mx-auto my-auto bg-yellow-500 rounded-none text-lg md:text-2xl text-black font-semibold px-6 md:px-10 py-4 md:py-7 hover:bg-white hover:shadow-2xl hover:shadow-orange-300"
-            >
-              HOW TO BUY $TTS
-            </Button>
-          </div>
+        {/* Launch Image and Button (Left Side) */}
+        <div className="lg:pl-[5%] lg:w-1/3 flex flex-col items-center z-40 space-y-4">
+          <Image
+            src={launch}
+            alt="launch"
+            className="w-full max-w-[250px] lg:max-w-[250px] h-auto"
+          />
+          <Button
+            onClick={scrollToSection}
+            className="w-full max-w-[250px] justify-center items-center bg-yellow-500 rounded-none text-lg md:text-2xl text-black font-semibold px-6 md:px-10 py-4 md:py-7 hover:bg-white hover:shadow-2xl hover:shadow-orange-300"
+          >
+            HOW TO BUY $TTS
+          </Button>
         </div>
 
-        {/* Hero Image (Proportionally Scaled and Positioned at Bottom) */}
-        <div className="lg:flex hidden absolute bottom-0 right-0 z-10">
+        {/* Hero Image (Right Side) */}
+        <div className="hidden lg:flex lg:w-2/3 justify-end items-end absolute bottom-0 right-0 z-10">
           <Image
             src={hero}
             alt="hero"
