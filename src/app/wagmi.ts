5f517f1b3cb120d468d0202fd8f8eaaa1c18d97b
0xDas <126429597+Callmedas69@users.noreply.github.com>
2024-11-03 01:24:42 +0700

    Update wagmi.ts

--- src/app/wagmi.ts
+++ src/app/wagmi.ts
@@ -1,29 +1,58 @@
-import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
-import { base } from 'wagmi/chains'; // add baseSepolia for testing
-import { coinbaseWallet } from 'wagmi/connectors';
- 
-export function getConfig() {
-  return createConfig({
-    chains: [base], // add baseSepolia for testing
-    connectors: [
-      coinbaseWallet({
-        appName: "OnchainKit",
-        preference: 'all',
-        version: '4',
-      }),
-    ],
-    storage: createStorage({
-      storage: cookieStorage,
-    }),
-    ssr: true,
-    transports: {
-      [base.id]: http(), // add baseSepolia for testing
-    },
-  });
-}
- 
-declare module 'wagmi' {
-  interface Register {
-    config: ReturnType<typeof getConfig>;
+'use client';
+
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  coinbaseWallet,
+  metaMaskWallet,
+  rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { useMemo } from 'react';
+import { http, createConfig } from 'wagmi';
+import { base, baseSepolia } from 'wagmi/chains';
+import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
+
+export function useWagmiConfig() {
+  const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? '';
+  if (!projectId) {
+    const providerErrMessage =
+      'To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable';
+    throw new Error(providerErrMessage);
   }
+
+  return useMemo(() => {
+    const connectors = connectorsForWallets(
+      [
+        {
+          groupName: 'Recommended Wallet',
+          wallets: [coinbaseWallet],
+        },
+        {
+          groupName: 'Other Wallets',
+          wallets: [rainbowWallet, metaMaskWallet],
+        },
+      ],
+      {
+        appName: 'onchainkit',
+        projectId,
+      },
+    );
+
+    const wagmiConfig = createConfig({
+      chains: [base, baseSepolia],
+      
+      // turn off injected provider discovery
+      multiInjectedProviderDiscovery: false,
+      connectors,
+      ssr: true,
+      transports: {
+        [base.id]: http(),
+        [baseSepolia.id]: http(),
+      },
+    });
+
+    console.log('Connectors:', wagmiConfig.connectors);
+
+
+    return wagmiConfig;
+  }, [projectId]);
 }
